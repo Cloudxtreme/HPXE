9edef6873bc38a3bf4a6573b1da67a72bb135b08
Torstein Hansen <tsh-github@techbee.no>
2012-09-20 22:10:18 +0200

    added some files

---
+++ tftpboot/ipxe/boot.php
@@ -0,0 +1,26 @@
+<?php
+echo "
+
+DEFAULT http://172.27.80.8/ipxe/menu.c32
+PROMPT 0
+MENU TITLE Eniro PXE Boot
+TIMEOUT 200
+TOTALTIMEOUT 6000
+ONTIMEOUT local
+
+LABEL local
+MENU LABEL (local)
+MENU DEFAULT
+LOCALBOOT 0
+
+LABEL esxi50
+ KERNEL /esxi50/mboot.c32
+ APPEND -c /esxi50/auto-test.cfg
+ MENU LABEL ESXi-5.0.0
+
+LABEL Cobbler
+ KERNEL /esxi50/mboot.c32
+ APPEND -c /esxi50/auto-test.cfg
+ MENU LABEL Cobbler 
+"
+?>
